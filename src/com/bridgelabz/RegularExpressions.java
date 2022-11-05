76d860a673b596c37e3b3e0c35e35468cddc5794
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-05 09:16:07 +0530

    Added : First name starts with Cap and has minimum 3 characters

---
+++ src/com/bridgelabz/RegularExpressions.java
@@ -0,0 +1,18 @@
+// UC1- Valid First Name & First name starts with Cap and has minimum 3 characters
+package com.bridgelabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpressions
+{
+    static Pattern pattern = Pattern.compile("^([A-Z]{1}[a-z]{3,})$");
+    public static void main(String[] args)
+    {
+        Matcher matcher = pattern.matcher("Nagraj");
+        if(matcher.matches())
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
