efc0bd2874396e6e60b0c4853cf2a614aee86413
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-06 14:47:47 +0530

    Added : Validate Pre-defined Password rules at least 1 Upper case

--- src/com/bridgelabz/RegularExpressions.java
+++ src/com/bridgelabz/RegularExpressions.java
@@ -1,4 +1,4 @@
-// UC7 - Validate Pre-defined Password rules. Rule3– Should have at least 1 numeric number
+// UC6 - Validate Pre-defined Password rules. Rule3– Should have at least 1 Upper Case
 package com.bridgelabz;
 
 import java.util.regex.Matcher;
@@ -6,10 +6,10 @@ import java.util.regex.Pattern;
 
 public class RegularExpressions
 {
-    static Pattern pattern = Pattern.compile("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}");
+    static Pattern pattern = Pattern.compile("^(?=.*?[a-z])(?=.*?[A-Z]).{8,}$");
     public static void main(String[] args)
     {
-        Matcher matcher = pattern.matcher("NanchalPanchal1234");
+        Matcher matcher = pattern.matcher("NagrajPanchal");
         if(matcher.matches())
             System.out.println(true);
         else
